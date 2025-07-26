c596d94274950767c8dcc6f061721897aae1731d
maram_hasri <meesama89434@gmail.com>
2025-07-26 08:51:25 +0300

    after adding request to requirments

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Elastic Email Configuration
-ELASTIC_EMAIL_API_KEY = os.getenv("ELASTIC_EMAIL_API_KEY", "CD0ABCFF3149695B1DD566CC69FC428FE3C0E5F8D59E04CAAB3FA63A268A69197DB91E1DDAFB7EE3C64B308EED201A1B")
+ELASTIC_EMAIL_API_KEY = os.getenv("ELASTIC_EMAIL_API_KEY", "")
 ELASTIC_EMAIL_FROM = os.getenv("ELASTIC_EMAIL_FROM", "your_verified_sender@yourdomain.com")
 ELASTIC_EMAIL_URL = "https://api.elasticemail.com/v4/emails"
 
-# Other configurations can be added here
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+# Other configurations
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./book_platform.db")
